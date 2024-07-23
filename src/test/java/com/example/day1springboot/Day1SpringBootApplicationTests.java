f10bd75f5b233148b93ee9426ac30d87a88f6165
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-07-23 14:33:58 +0300

    Add files via upload

---
+++ src/test/java/com/example/day1springboot/Day1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
